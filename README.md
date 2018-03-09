d617e0a82d10a7268ac92b66f51cf6a3a3c59e06
alexander.ng <alexanderng@Alexander-Ng.local>
2018-03-09 08:45:14 -0800

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+### A Text to Speech Model for our Math Project
+
+A neural net to train it using data fed by audio files
--- neuralnet/neuralnet.py
+++ neuralnet/neuralnet.py
@@ -6,6 +6,7 @@ import math
 
 
 # Get input data
+# Currently using MNIST data as a placeholder, will replace at a later time.
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("tmp/data", one_hot=True)
 
